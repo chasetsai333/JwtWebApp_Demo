f2802efff0660614aae6a5e3b837d83d35a6b0a5
chase.tsai <chasetsai333@gmail.com>
2023-04-19 22:41:40 +0800

    [feat] Swagger UI 增加驗證按鈕、Header帶Token

--- JwtWebApp/Program.cs
+++ JwtWebApp/Program.cs
@@ -1,5 +1,6 @@
 using JwtWebApp.Infrastructure.Helpers;
 using JwtWebApp.Infrastructures.Extensions;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +8,35 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen((options) => 
+{
+    options.AddSecurityDefinition("Bearer",
+    new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization"
+    });
+
+    options.AddSecurityRequirement(
+        new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] {}
+            }
+        });
+});
 builder.Services.AddAuthByJwt(builder.Configuration);
 builder.Services.AddSingleton<JwtHelper>();
 
