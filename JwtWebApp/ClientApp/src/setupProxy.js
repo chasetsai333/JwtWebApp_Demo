0bbfeda58bc9569fa70d2cdca0c1f7465dfc4716
chase.tsai <chasetsai333@gmail.com>
2023-04-19 15:05:16 +0800

    [chore] change proxy target

--- JwtWebApp/ClientApp/src/setupProxy.js
+++ JwtWebApp/ClientApp/src/setupProxy.js
@@ -2,10 +2,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:56193';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:44384';
 
 const context =  [
-  "/weatherforecast",
+  "/WeatherForecast",
 ];
 
 module.exports = function(app) {
